77b70cca89452abd4bc2e9138029160abfe2b045
Austin Lang <austin.f.lang@gmail.com>
2022-05-02 21:01:27 -0400

    adding more to model.py

--- model.py
+++ model.py
@@ -1,33 +1,25 @@
+import math
 import numpy as np
 import tensorflow as tf
+from preprocess import get_data
 
 class Feed_Forwards(tf.keras.layers.Layer):
 	def __init__(self, emb_sz, ff_dim):
 		super(Feed_Forwards, self).__init__()
-
-		self.layer_1 = tf.keras.layers.Dense(ff_dim,activation='relu')
+		self.layer_1 = tf.keras.layers.Dense(ff_dim, activation='relu')
 		self.layer_2 = tf.keras.layers.Dense(emb_sz)
 
 	@tf.function
 	def call(self, inputs):
-		"""
-		This functions creates a feed forward network as described in 3.3
-		https://arxiv.org/pdf/1706.03762.pdf
-		Requirements:
-		- Two linear layers with relu between them
-		:param inputs: input tensor [batch_size x window_size x embedding_size]
-		:return: tensor [batch_size x window_size x embedding_size]
-		"""
 		layer_1_out = self.layer_1(inputs)
 		layer_2_out = self.layer_2(layer_1_out)
 		return layer_2_out
 
 class Transformer_Block(tf.keras.layers.Layer):
-    def __init__(self, emb_sz, num_heads, ff_dim, rate=0.1):
+    def __init__(self, emb_sz, num_heads, ff_dim, rate=0.3):
         super(Transformer_Block, self).__init__()
-        self.att = tf.keras.layers.MultiHeadAttention(num_heads = num_heads, key_dim = emb_sz)
+        self.att = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=emb_sz)
         self.ff_layer = Feed_Forwards(emb_sz, ff_dim)
-        #episilon is 1e-3 by defaut, use default?
         self.layer_norm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layer_norm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.dropout1 = tf.keras.layers.Dropout(rate)
@@ -35,26 +27,6 @@ class Transformer_Block(tf.keras.layers.Layer):
     
     @tf.function
     def call(self, inputs, training):
-        """
-		This functions calls a transformer block.
-		There are two possibilities for when this function is called.
-		    - if self.is_decoder == False, then:
-		        1) compute unmasked attention on the inputs
-		        2) residual connection and layer normalization
-		        3) feed forward layer
-		        4) residual connection and layer normalization
-		    - if self.is_decoder == True, then:
-		        1) compute MASKED attention on the inputs
-		        2) residual connection and layer normalization
-		        3) computed UNMASKED attention using context
-		        4) residual connection and layer normalization
-		        5) feed forward layer
-		        6) residual layer and layer normalization
-		If the multi_headed==True, the model uses multiheaded attention (Only 2470 students must implement this)
-		:param inputs: tensor of [BATCH_SIZE x (ENG/FRN)_WINDOW_SIZE x EMBEDDING_SIZE ]
-		:context: tensor of [BATCH_SIZE x FRENCH_WINDOW_SIZE x EMBEDDING_SIZE ] or None
-			default=None, This is context from the encoder to be used as Keys and Values in self-attention function
-		"""
         attn_output = self.att(inputs, inputs)
         attn_output = self.dropout1(attn_output, training=training)
         out1 = self.layer_norm1(inputs + attn_output)
@@ -63,114 +35,109 @@ class Transformer_Block(tf.keras.layers.Layer):
         return self.layer_norm2(out1 + ffl_output)
 
 class Position_Encoding_Layer(tf.keras.layers.Layer):
-	def __init__(self, window_sz, emb_sz):
+	def __init__(self, emb_sz):
 		super(Position_Encoding_Layer, self).__init__()
-		self.positional_embeddings = self.add_weight("pos_embed",shape=[window_sz, emb_sz])
+		self.token_embeddings = tf.keras.layers.Embedding(emb_sz, emb_sz)
+		self.pos_embeddings = tf.keras.layers.Embedding(emb_sz, emb_sz)
 
 	@tf.function
 	def call(self, x):
-		"""
-		Adds positional embeddings to word embeddings.
-		:param x: [BATCH_SIZE x (ENG/FRN)_WINDOW_SIZE x EMBEDDING_SIZE ] the input embeddings fed to the encoder
-		:return: [BATCH_SIZE x (ENG/FRN)_WINDOW_SIZE x EMBEDDING_SIZE ] new word embeddings with added positional encodings
-		"""
-		return x+self.positional_embeddings
+		positional_encoding = self.positional_embeddings(x)
+		token_encoding = self.token_embeddings(x)
+		return positional_encoding + token_encoding
 
-class Transformer_Seq2Seq(tf.keras.Model):
-	def __init__(self, french_window_size, french_vocab_size, english_window_size, english_vocab_size):
+class Decoding_Layer(tf.keras.layers.Layer):
+	def __init__(self, emb_sz, num_heads, ff_dim, rate=0.3):
+		super(Decoding_Layer, self).__init__()
+		self.decoder_layer = Transformer_Block(emb_sz, num_heads, ff_dim)
+		self.layer_norm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+		self.dropout = tf.keras.layers.Dropout(rate)
 
-		######vvv DO NOT CHANGE vvv##################
-		super(Transformer_Seq2Seq, self).__init__()
+def train(model, train_inputs, train_labels, pad_id):
+	for i in range(0, len(train_inputs), model.batch_size):
+		input_batch = train_inputs[i:i+model.batch_size]
+		label_batch = train_labels[i:i+model.batch_size]
+		mask = np.where(label_batch == pad_id, 0, 1)
+		mask = tf.convert_to_tensor(mask, dtype=tf.float32)
+		if (len(train_labels) != model.batch_size):
+			return
+		with tf.GradientTape() as tape:
+			probabilities = model.call(input_batch, training=True)
+			loss = model.loss(probabilities, label_batch, mask)
+		gradients = tape.gradient(loss, model.trainable_variables)
+		model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-		self.french_vocab_size = french_vocab_size # The size of the French vocab
-		self.english_vocab_size = english_vocab_size # The size of the English vocab
+def test(model, test_inputs, test_labels, pad_id):
+	total_loss = 0
+	total_accuracy = 0
+	total_non_padded = 0
+	for i in range(0, len(test_inputs), model.batch_size):
+		input_batch = test_inputs[i:i+model.batch_size]
+		label_batch = test_labels[i:i+model.batch_size]
+		mask = np.where(label_batch == pad_id, 0, 1)
+		non_padded = np.sum(mask)
+		mask = tf.convert_to_tensor(mask, dtype=tf.float32)
+		total_non_padded += non_padded
+		if (len(test_labels) != model.batch_size):
+			return
+		probabilities = model.call(input_batch, training=False)
+		loss = model.loss(probabilities, label_batch, mask)
+		total_loss += loss
+		total_accuracy += model.accuracy(probabilities, label_batch, mask)
+	return math.exp(total_loss / total_non_padded), (total_accuracy / total_non_padded)	
 
-		self.french_window_size = french_window_size # The French window size
-		self.english_window_size = english_window_size # The English window size
-		######^^^ DO NOT CHANGE ^^^##################
-
-
-		# TODO:
-		# 1) Define any hyperparameters
-		# 2) Define embeddings, encoder, decoder, and feed forward layers
-
-		# Define batch size and optimizer/learning rate
+class Model(tf.keras.Model):
+	def __init__(self, ):
+		super(Model, self).__init__()
 		self.batch_size = 100
-		self.embedding_size = 200
-		self.optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+		self.embedding_size = 1000
+		self.num_heads = 10
+		self.ff_dim = 2000
+		
+		self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
-		# Define English and French embedding layers:
-		self.emb_e = tf.keras.layers.Embedding(self.english_vocab_size, self.embedding_size)
-		self.emb_f = tf.keras.layers.Embedding(self.french_vocab_size, self.embedding_size)
-
-		# Create positional encoder layers
-		self.pos_e = Position_Encoding_Layer(self.english_window_size, self.embedding_size)
-		self.pos_f = Position_Encoding_Layer(self.french_window_size, self.embedding_size)
-
-		# Define encoder and decoder layers:
-		self.encoder = Transformer_Block(self.embedding_size, False)
-		self.decoder = Transformer_Block(self.embedding_size, True)
-
-		# Define dense layer(s)
-		self.ffl = tf.keras.layers.Dense(self.english_vocab_size)
+		self.feed_forwards = Feed_Forwards(self.embedding_size, self.ff_dim)
+		self.encoder_layer = Position_Encoding_Layer(self.embedding_size)
+		self.decoder_layer = Decoding_Layer(self.embedding_size, self.num_heads, self.ff_dim)
 
 	@tf.function
-	def call(self, encoder_input, decoder_input):
-		"""
-		:param encoder_input: batched ids corresponding to French sentences
-		:param decoder_input: batched ids corresponding to English sentences
-		:return prbs: The 3d probabilities as a tensor, [batch_size x window_size x english_vocab_size]
-		"""
-
-		# TODO:
-		#1) Add the positional embeddings to French sentence embeddings
-		#2) Pass the French sentence embeddings to the encoder
-		#3) Add positional embeddings to the English sentence embeddings
-		#4) Pass the English embeddings and output of your encoder, to the decoder
-		#5) Apply dense layer(s) to the decoder out to generate probabilities
-
-		f_input = self.pos_f(self.emb_f(encoder_input))
-		output = self.encoder(f_input)
-		e_input = self.pos_e(self.emb_e(decoder_input))
-		output = self.decoder(e_input, output)
-		output = self.ffl(output)
-		probabilities = tf.nn.softmax(output)
-
-		return probabilities
+	def call(self, encoder_input):
+		encoder_output = self.encoder_layer(encoder_input)
+		decoder_output = self.decoder_layer(encoder_output)
+		ffl_output = self.feed_forwards(decoder_output)
+		return tf.nn.softmax(ffl_output)
 
 	def accuracy_function(self, prbs, labels, mask):
-		"""
-		DO NOT CHANGE
-		Computes the batch accuracy
-
-		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
-		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
-		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
-		:return: scalar tensor of accuracy of the batch between 0 and 1
-		"""
-
 		decoded_symbols = tf.argmax(input=prbs, axis=2)
 		accuracy = tf.reduce_mean(tf.boolean_mask(tf.cast(tf.equal(decoded_symbols, labels), dtype=tf.float32),mask))
 		return accuracy
 
-
 	def loss_function(self, prbs, labels, mask):
-		"""
-		Calculates the model cross-entropy loss after one forward pass
-		Please use reduce sum here instead of reduce mean to make things easier in calculating per symbol accuracy.
-		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
-		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
-		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
-		:return: the loss of the model as a tensor
-		"""
-
-		# Note: you can reuse this from rnn_model.
-
 		loss = tf.keras.losses.sparse_categorical_crossentropy(labels, prbs)
 		loss = tf.boolean_mask(loss, mask)
 		loss = tf.reduce_sum(loss)
-
 		return loss
 
 	def __call__(self, *args, **kwargs):
-		return super(Transformer_Seq2Seq, self).__call__(*args, **kwargs)
+		return super(Model, self).__call__(*args, **kwargs)
+
+def main():
+	epochs = 10
+
+	model = Model()
+
+	data = get_data()
+	train_inputs = data[0]
+	train_labels = data[1]
+	test_inputs = data[2]
+	test_labels = data[3]
+	pad_id = data[4]
+	word_id = data[5]
+	
+	for i in range(0, epochs):
+		train(model, train_inputs, train_labels, pad_id)
+
+	print(test(model, test_inputs, test_labels, pad_id))
+
+if __name__ == '__main__':
+    main()
--- preprocess.py
+++ preprocess.py
@@ -6,16 +6,13 @@ from collections import Counter
 import numpy as np
 import functools
 
-
 initial_examples = 20000
 train_num = 15000
-
 
 def only_alpha(text):
     "Makes it so that the string entered only returns ascii, alphabetically characters or spaces"
     alpha = "".join([i for i in text if (i.isalpha() and i.isascii()) or i == ' '])
     return alpha.upper()
-
 
 def go_to_100(l):
     '''Makes it so that the list will always have a length of 100. If it's too large it'll be truncated.
@@ -37,35 +34,32 @@ def get_data():
     # Format of webmd is Age, Condition, Date, Drug, DrugId, Ease of Use, Effectiveness, Satisfaction, Sex, Side Effects, Useful Count
     # Want Age, Condition, DrugId,  Ease of Use, Effectiveness, Satisfaction, Sex, Useful Count 
 
-
     # drug = webmd["Drug"]
     # relevant_drugs = Counter(drug.tolist()).most_common(50)
 
-
-
     # relevant_drugs = list(map(lambda x: x[0], relevant_drugs)) # extracts the name of the 50 most common drugs reviewed
 
-    # # Relevant drugs may later be converted to ID form to do stuff 
-    # # webmd = webmd[webmd.Drug.isin(relevant_drugs)] #106389 examples
+    # Relevant drugs may later be converted to ID form to do stuff 
+    # webmd = webmd[webmd.Drug.isin(relevant_drugs)] #106389 examples
 
     reviews = webmd["Reviews"].to_list()
 
-    #Average sentence is 75-100 characters, filters out reviews that are too short
+    # Average sentence is 75-100 characters, filters out reviews that are too short
     review_list = []
 
     for review in reviews:
         if (type(review) is str) and (len(list(review)) >= 75):    
             review_list += [review]
 
-    #print(len(review_list)) #271509 examples left
+    # print(len(review_list)) #271509 examples left
 
-    #makes it so there's only initial_example number of reviews left
+    # makes it so there's only initial_example number of reviews left
     review_list = review_list[:initial_examples]
 
-    #filters webmd so there's only values with review_list in it
+    # filters webmd so there's only values with review_list in it
     webmd = webmd[webmd.Reviews.isin(review_list)]
 
-    #Adding Ease of Use, Effectiveness, Satisfaction together
+    # Adding Ease of Use, Effectiveness, Satisfaction together
     score =  np.array(webmd['EaseofUse'].to_list()) + np.array(webmd['Effectiveness'].to_list()) + np.array(webmd['Satisfaction'].to_list())
 
     # classifying the scores to negative 0, neutral 1, and positive 2
@@ -73,13 +67,13 @@ def get_data():
     score = np.where(score > 11, 2, score) # score is 2 if greater than 11 (positive)
     score = np.where(score > 6, 1, score) # if score is greater than 6 and less than 12 (neutral )
 
-    #just to make sure that reviews are in the same order as the scores
+    # just to make sure that reviews are in the same order as the scores
     reviews = webmd['Reviews'].to_list()
 
-    #makes it so it's only alphabetical characters
+    # makes it so it's only alphabetical characters
     reviews = list(map(only_alpha, reviews))
 
-    #reviews = reviews[:10]
+    # reviews = reviews[:10]
 
     reviews = [x.split(" ") for x in reviews]
 
@@ -87,11 +81,10 @@ def get_data():
 
     reviews = list(map(go_to_100, reviews))
 
-    #flattens the list of list to a list
+    # flattens the list of list to a list
     words = functools.reduce(lambda x,y: x + y, reviews)
 
-
-    #dictionary that stores the translataion of word to id
+    # dictionary that stores the translation of word to id
     word_id = {} 
 
     count = 0
@@ -109,25 +102,21 @@ def get_data():
             
     pad_id = word_id['PADDING']
 
-
     train_inputs = corpus_id[:train_num]
     train_labels = score[:train_num]
 
     test_inputs = corpus_id[train_num:]
     test_labels = score[train_num:]
 
-
     # print(test_inputs.shape)
-
-    # print(train_inputs.shape)  #(10000, 100)
+    # print(train_inputs.shape) #(10000, 100)
     # print(train_labels.shape) #(10000, 100)
-    #print(test_inputs.shape) (5204, 100)
-    #print(test_labels.shape) (5204,)
-    #print(pad_id)
-    #print(word_id)
+    # print(test_inputs.shape) (5204, 100)
+    # print(test_labels.shape) (5204,)
+    # print(pad_id)
+    # print(word_id)
 
     return (train_inputs, train_labels, test_inputs, test_labels, pad_id, word_id)
-
 
 # def main():
 # 	get_data()
