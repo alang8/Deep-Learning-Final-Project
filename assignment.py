e812877113c3f0eb36bac3c825b0d8cfb0efc76b
Austin Lang <austin.f.lang@gmail.com>
2022-05-11 19:07:07 -0400

    edited code style

--- assignment.py
+++ assignment.py
@@ -9,12 +9,11 @@ import os
 from lime.lime_text import LimeTextExplainer
 import matplotlib.pyplot as plt
 
-# pip install lime and pip install ipython
+### How to Run ###
+# run class environment
+# pip install pandas, lime and ipython
 
-# ensures that we run only on cpu
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
-# this file does binary classification
+# this implementation does binary classification (positive vs negative)
 
 class Model(tf.keras.Model):
     def __init__(self):
@@ -37,7 +36,7 @@ def create_model():
 
 
 def main():
-
+    
     all_inputs, all_labels, tokenizer = get_data()
     dir_exists = os.path.isdir('binary_model')
 
@@ -50,6 +49,7 @@ def main():
 
         h = model.fit(all_inputs, all_labels, validation_split=0.2, epochs=1, batch_size=1024) # 
         # loss: 0.4719 - accuracy: 0.7735 - val_loss: 0.4315 - val_accuracy: 0.8003
+        
         model.save("binary_model")
     else:
         model = tf.keras.models.load_model("binary_model")
@@ -74,40 +74,12 @@ def main():
     class_names=['negative','positive']
     explainer= LimeTextExplainer(class_names=class_names)
 
-    # counter = 0
-
-    # with open('binary_reviews.txt', 'r') as f:
-    #     r_list = f.readlines()
-
-    # for i in range(0, 1000, 2):
-    #     review = r_list[i]
-        
-    #     review = ' '.join(review.split())
-    #     exp = explainer.explain_instance(review, predict_proba)
-        
-    #     html = "LIME_pictures/" + str(counter) + r_list[i + 1].strip() + ".html"
-    #     exp.save_to_file(html)
-
-    #     print(counter)
-
-    #     counter += 1
-
     ### Lime Example ###
     x = 'my grandfather was prescribed this medication coumadin to assist in blood thinning due to a heart and thyroid condition his primary doctor was aware that he was on an aspirin regiment and still prescribed this medicine it caused his blood to thin out to much and he ended up internally bleeding to death if you are going to take this medicine please ask your doctors about possible side effects or drug interactions'
     x = ' '.join(x.split())
     exp = explainer.explain_instance(x, predict_proba)
     exp.save_to_file('lime1.html') # has to be html 
 
-    ### This is just to check that the data loads properly. Can ignore ###
-    # indicies = np.random.choice(len(all_inputs), 54302, replace=False)
-
-    # test_inputs = all_inputs[indicies]
-    # test_labels = all_labels[indicies]
-
-    # model = tf.keras.models.load_model("binary_model")
-
-    # score = model.evaluate(test_inputs, test_labels, verbose=0)
-    # print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
 
 if __name__ == "__main__":
     main()
--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import re
 
-# this file does binary classification
+# this implementation does binary classification (positive vs negative)
 
 def only_alpha(text):
     "Makes it so that the string entered only returns ascii, alphabetically characters or spaces"
@@ -26,8 +26,6 @@ def get_data():
         if (type(review) is str) and (len(list(review)) >= 75):    
             review_list += [review]
 
-    # print(len(review_list)) #271509 examples left
-
     # makes it so there's only initial_example number of reviews left
     review_list = review_list
 
@@ -37,48 +35,26 @@ def get_data():
     # adding ease of use, effectiveness, satisfaction together
     scores =  np.array(webmd['EaseofUse'].to_list()) + np.array(webmd['Effectiveness'].to_list()) + np.array(webmd['Satisfaction'].to_list())
 
-    # classifying the scores to negative 0, neutral 1, and positive 2
-    # score = np.where(score < 7, 0, score) #score is 0 if less than 7 (negative)
-    # score = np.where(score > 11, 2, score) # score is 2 if greater than 11 (positive)
-    # score = np.where(score > 6, 1, score) # if score is greater than 6 and less than 12 (neutral )
+    # classifying the scores to negative and positive
+    # score is 0 if less than 9 (negative), 1 if greater than equal to 9 (positive)
 
-    score = np.where(scores < 9, 0, 1) #score is 0 if less than 9 (negative), 1 if greater than equal to 9, (positive)
+    score = np.where(scores < 9, 0, 1)
 
     # just to make sure that reviews are in the same order as the scores
     reviews = webmd['Reviews'].to_list()
 
-    reviews = list(map(lambda x: re.sub(r"\s+", " ", x), reviews)) # replaces tabs/newlines with spaces
+    reviews = list(map(lambda x: re.sub(r"\s+", " ", x), reviews)) # replaces tabs and newlines with spaces
     reviews = list(map(lambda x: re.sub(' +', ' ', x), reviews)) # removes repeating spaces
 
-    # makes it so it's only alphabetical characters
+    # adjust review so that it only includes alphabetical characters
     reviews = list(map(only_alpha, reviews))
 
-    ### This is just to see the reviews and for LIME ###
-    # with open("binary_reviews.txt", "w") as f:
-    #     for review, s, sentiment  in zip(reviews, scores, score):
-    #         f.write(review + '\n') #+ '\n')
-    #         f.write("Score is " + s.astype(str) + '\n')
-
-    #         if s < 7: 
-    #             sent = "Negative"
-    #         elif s > 11:
-    #             sent = "Positive"
-    #         else:
-    #             sent = "Neutral"
-
-            # if sentiment == 1:
-            #     sent2 = "Positive"
-            # else:
-            #     sent2 = "Negative"
-
-            # #f.write("Sentiment for Multi is " + sent + '\n')
-            # f.write(sent2 + '\n')
-
+    # tokenizes review
     tokenizer = Tokenizer(num_words = 20000, oov_token="oov")
     tokenizer.fit_on_texts(reviews)
 
+    # encodes and pads review
     encoded_docs = tokenizer.texts_to_sequences(reviews)
-
     padded_sequence = pad_sequences(encoded_docs, maxlen=200)
 
     return (padded_sequence, score, tokenizer)
